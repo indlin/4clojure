e9b54a14d7b72342bb95c55d083c4147741fa112
Sergey Indlin <indlin@gmail.com>
2015-09-16 14:24:17 +0300

    Move tests to the one file

--- src/solve4clojure/nth_my.clj
+++ src/solve4clojure/nth_my.clj
@@ -1,10 +1,7 @@
 (ns solve4clojure.nth-my)
 ;; #21
 
-(defn
-  ^{:test (fn []
-            (assert (= (nth-my '(4 5 6 7) 2) 6)))}
-  nth-my [coll x]
+(defn nth-my [coll x]
   (if (zero? x)
     (first coll)
     (recur (next coll) (dec x))))
--- test/solve4clojure/core_test.clj
+++ test/solve4clojure/core_test.clj
@@ -1,3 +1,15 @@
 (ns solve4clojure.core-test
   (:require [clojure.test :refer :all]
-            [solve4clojure.core :refer :all]))
+            [solve4clojure.prime :as prime]
+            [solve4clojure.nth-my :as nth]))
+
+(deftest prime-test
+  (testing "prime"
+    (is (= (prime/get-prime 2) [2 3]))
+    (is (= (prime/get-prime 5) [2 3 5 7 11]))
+    (is (= (last (prime/get-prime 100)) 541))))
+
+(deftest nth-test
+  (testing "nth-my."
+    (is (= (nth/nth-my '(4 5 6 7) 2) 6))
+    (is (= (nth/nth-my [:a :b :c] 0) :a))))
